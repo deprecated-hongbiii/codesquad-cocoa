99c02ced1918ccfe5bccec37a430273df6c4e9a0
hongbi <dyongdi@gmail.com>
2020-11-03 17:27:39 +0900

    fix: constructor에 인자를 추가해 알맞은 도형 이름으로 출력되도록 변경

--- day2/area.js
+++ day2/area.js
@@ -1,5 +1,6 @@
 class Quadrangle {
-  constructor(height, width1, width2 = width1) {
+  constructor(shape, height, width1, width2 = width1) {
+    this.shape = shape;
     this.height = height;
     this.width1 = width1;
     this.width2 = width2;
@@ -7,12 +8,11 @@ class Quadrangle {
 
   getArea() {
     const width = (this.width1 + this.width2) / 2;
-    console.log(this);
-    console.log(`${JSON.stringify(this)}의 넓이는 ${width * this.height}`);
+    console.log(`${this.shape}의 넓이는 ${width * this.height}`);
   }
 }
 
-const rectangle = new Quadrangle(5, 6);
-const trapezoid = new Quadrangle(4, 8, 6);
+const rectangle = new Quadrangle('사각형', 5, 6);
+const trapezoid = new Quadrangle('사다리꼴', 4, 8, 6);
 rectangle.getArea();
 trapezoid.getArea();
