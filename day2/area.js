9f49c19dc593392ef60c99f82f6886ac31316296
hongbi <dyongdi@gmail.com>
2020-11-03 17:58:16 +0900

    feat: printExecutionSequence 함수 추가

--- day2/area.js
+++ day2/area.js
@@ -1,3 +1,14 @@
+let executionSequence = [];
+
+const saveExecutionSequence = (data) => {
+  executionSequence.push(data);
+};
+
+const printExecutionSequence = () => {
+  const result = executionSequence.join(', ');
+  console.log(`[함수 실행 로그]: ${result}`);
+};
+
 class Quadrangle {
   constructor(shape, height, width1, width2 = width1) {
     this.shape = shape;
@@ -9,13 +20,9 @@ class Quadrangle {
   getArea() {
     const width = (this.width1 + this.width2) / 2;
     console.log(`${this.shape}의 넓이는 ${width * this.height}`);
+    saveExecutionSequence(`${this.shape}`);
   }
 }
-
-const rectangle = new Quadrangle('사각형', 5, 6);
-const trapezoid = new Quadrangle('사다리꼴', 4, 8, 6);
-rectangle.getArea();
-trapezoid.getArea();
 
 const PI = 3.14;
 
@@ -26,6 +33,7 @@ class Circle {
 
   getArea() {
     console.log(`원의 넓이는 ${Math.pow(this.radius, 2) * PI}`);
+    saveExecutionSequence('원');
   }
 
   getAreaSum(n) {
@@ -35,9 +43,20 @@ class Circle {
       sum = sum + area;
     }
     console.log(`반지름이 1부터 n까지 n개의 원의 넓이 합은 ${sum}`);
+    saveExecutionSequence('원의 넓이의 합');
   }
 }
 
 const circle = new Circle(3);
-circle.getArea();
-circle.getAreaSum(3);
+const rectangle = new Quadrangle('사각형', 5, 6);
+const trapezoid = new Quadrangle('사다리꼴', 4, 8, 6);
+
+function init() {
+  rectangle.getArea();
+  trapezoid.getArea();
+  circle.getArea();
+  circle.getAreaSum(3);
+  printExecutionSequence();
+}
+
+init();
