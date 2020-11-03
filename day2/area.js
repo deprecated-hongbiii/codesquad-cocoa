b7f7b29bf86f6663b2f7d561867646c73c0106bf
hongbi <dyongdi@gmail.com>
2020-11-03 17:38:30 +0900

    feat: 원 class 정의 및 생성

--- day2/area.js
+++ day2/area.js
@@ -16,3 +16,28 @@ const rectangle = new Quadrangle('사각형', 5, 6);
 const trapezoid = new Quadrangle('사다리꼴', 4, 8, 6);
 rectangle.getArea();
 trapezoid.getArea();
+
+const PI = 3.14;
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  getArea() {
+    console.log(`원의 넓이는 ${Math.pow(this.radius, 2) * PI}`);
+  }
+
+  getAreaSum(n) {
+    let sum = 0;
+    for (let i = 1; i <= this.radius; i++) {
+      const area = Math.pow(i, 2) * PI;
+      sum = sum + area;
+    }
+    console.log(`반지름이 1부터 n까지 n개의 원의 넓이 합은 ${sum}`);
+  }
+}
+
+const circle = new Circle(3);
+circle.getArea();
+circle.getAreaSum(3);
