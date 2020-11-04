69325323aba4119acdace76c890f0851b6feefcd
hongbi <dyongdi@gmail.com>
2020-11-04 12:34:49 +0900

    rename: 파일 이름 변경

---
+++ day3/binary_1.js
@@ -0,0 +1,23 @@
+let binaryArr = [];
+let result = [];
+
+const solution = (n, t, m) => {
+  const TOTAL_NUM = t * m;
+
+  const changeToBinary = (TOTAL_NUM) => {
+    for (let i = 0; i < TOTAL_NUM; i++) {
+      let binary = i.toString(2);
+      binaryArr.push(binary);
+    }
+  };
+
+  changeToBinary(TOTAL_NUM);
+
+  for (let elem of binaryArr) {
+    let num = elem.split('');
+    result.push(...num);
+  }
+  console.log(result);
+};
+
+solution(2, 4, 2);
