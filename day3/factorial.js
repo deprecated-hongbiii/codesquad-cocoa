b38ddd2ca850b4ae767b5eeac98d21f290e4f670
hongbi <dyongdi@gmail.com>
2020-11-04 16:26:04 +0900

    day3_미션2_문제1_factorial 함수

---
+++ day3/factorial.js
@@ -0,0 +1,18 @@
+let factorialArr = [];
+
+const factorial = (number) => {
+  if (number > 0) {
+    return number * factorial(number - 1);
+  } else {
+    return 1;
+  }
+};
+
+const calculate = (m) => {
+  for (let i = 1; i <= m; i++) {
+    factorialArr.push(factorial(i));
+  }
+};
+
+calculate(4);
+console.log(factorialArr);
