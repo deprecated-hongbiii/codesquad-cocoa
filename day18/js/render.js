ab7b77d4fadffc71ceaed87a329b86c405b3fb79
Hongbi <dyongdi@gmail.com>
2020-11-25 19:05:19 +0900

    feat: 랜덤으로 마을 총 개수 결정, 그 수만큼 마을 객체 생성

--- day18/js/render.js
+++ day18/js/render.js
@@ -1,28 +1,58 @@
+function getRandomInt(min, max) { // min 이상 max 미만의 정수
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 class Village {
   constructor(name) {
     this.name = name;
-    this.postbox = false,
-    this.postBoxSize = this.getRandomInt(0, 101);
+    this.postbox = this.hasPostBox();
+    this.postBoxSize = this.postbox === false ? null : getRandomInt(0, 101);
   }
   
   hasPostBox() {
     // 일정 확률로 postbox 유무를 true/false 로 정해줌.
     // 랜덤 수 돌려서 8 이상이면 true.
-    const randomInt = this.getRandomInt(0, 11);
+    const randomInt = getRandomInt(0, 11);
     if(randomInt > 8) {
-      this.postbox = true;
+      return this.postbox = true; // 나중에 true이면 우체통 ui 넣어주기
     } else {
-      this.postbox = false;
+      return this.postbox = false;
     }
-  }
-  
-  getRandomInt(min, max) { // min 이상 max 미만의 정수
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
   }
 }
 
-const a = new Village('A', 1);
-a.hasPostBox();
-console.log(a);
+class RandomVillage {
+  // 랜덤으로 어떻게 짤건지 로직
+  createVillage() {
+    const randomInt = getRandomInt(0, 11);
+    if(randomInt < 3) {
+      console.log('이전 마을의 형제')
+    } else if(randomInt >= 3 && randomInt < 8) {
+      console.log('이전 마을의 자식')
+    } else {
+      console.log('새로운 마을 덩어리')
+    }
+  }
+}
+
+class View {
+  constructor() {
+    // RandomVillage 의 결과값을 가져온다 ?
+  }
+}
+
+const map = document.querySelector('.map');
+map.innerHTML = `<div class="village" id="a">A</div>`;
+const prev = document.querySelector('#a');
+
+const template = `<div class="village" id="${this.name}">${this.name}</div>`
+
+const TOTAL_VILLAGE = getRandomInt(2, 27);
+let villages =[];
+for(let i = 0; i < TOTAL_VILLAGE; i++) {
+  const village = new Village(String.fromCharCode(65 + i));
+  villages.push(village);
+}
+console.log(villages);
